3408f562e47c209d86ade68c499b1caed7269f2b
Sreeram Athrij <sreeramathrij344@gmail.com>
2023-11-04 19:42:09 +0530

    home page and index made
    have to code content in the index.html file

--- callisto/callisto/settings.py
+++ callisto/callisto/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = 'static/'
+
+#Add this in your settings.py file:
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
--- callisto/home/models.py
+++ callisto/home/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class React(models.Model):
-    user = models.CharField(max_length=50)
-    department = models.CharField(max_length=100)
